0754ccdc7d4c0f46d0a3b8c052470e88d3448317
emran-iths <155668324+emran-iths@users.noreply.github.com>
2024-01-18 20:18:43 +0100

    added styled components. and a title using styled components

--- src/App.js
+++ src/App.js
@@ -9,23 +9,33 @@ import { NotFound } from "./pages/NotFound";
 import LongLatContext from "./LongLatContext";
 import Navbar from "./Navbar";
 import Map from "./Map";
+import styled from "styled-components";
 
 function App() {
   const longLatState = useState(null);
   const [longLat, setLongLat] = longLatState;
 
+  const Title = styled.h1`
+    font-size: 24;
+    text-align: center;
+    color: #bb0099;
+  `;
+
   return (
-    <LongLatContext.Provider value={longLatState}>
-      <Map />
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Navigate to="/temperature" />} />
-        <Route path="/temperature" element={<Temperature />} />
-        <Route path="/wind_speed" element={<WindSpeed />} />
-        <Route path="/humidity" element={<Humidity />} />
-        <Route path="/*" element={<NotFound />} />
-      </Routes>
-    </LongLatContext.Provider>
+    <>
+      <Title>Weather app</Title>
+      <LongLatContext.Provider value={longLatState}>
+        <Map />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Navigate to="/temperature" />} />
+          <Route path="/temperature" element={<Temperature />} />
+          <Route path="/wind_speed" element={<WindSpeed />} />
+          <Route path="/humidity" element={<Humidity />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </LongLatContext.Provider>
+    </>
   );
 }
 
